5567064b730ceda3579c55b1966a8c148eebc85a
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-03-16 09:41:35 -0600

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -8,11 +8,5 @@ function helloWorld() {
 // }
 
 function sayHello(name) {
-    if (name == "Alex") {
-        return "Hello, Alex!";
-    } else if (name == "Pat") {
-        return "Hello, Pat!";
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + name + "!";
 }
